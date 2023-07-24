c74c37b844c2521c219fdb26f934fda37d08a1dc
Huu Phuoc <s3878270@rmit.edu.vn>
2023-07-24 14:11:12 +0700

    Initial Commit

---
+++ A1/A1App.swift
@@ -0,0 +1,17 @@
+//
+//  A1App.swift
+//  A1
+//
+//  Created by Hữu Phước  on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct A1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
